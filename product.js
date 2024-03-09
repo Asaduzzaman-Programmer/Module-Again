a67a4c12d048b33f8e2a24c0e0fab60247a2c4f0
asad <asaduzzamanprogrammer@gmail.com>
2024-03-09 14:43:11 +0600

    add

---
+++ product.js
@@ -0,0 +1,11 @@
+const addProduct = () =>{
+    const nameo = document.getElementById("name");
+    const quantityo = document.getElementById("quantity");
+    const jh = nameo.value;
+    const jh2 = quantityo.value;
+
+    nameo.value = '';
+    quantityo.value = '';
+    const product = console.log(jh, jh2);
+    
+}
